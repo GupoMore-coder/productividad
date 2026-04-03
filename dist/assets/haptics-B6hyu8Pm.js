64023e133d6b889a8495c6c2b2eabc10c6046a00
GupoMore-coder <fernando830609@gmail.com>
2026-04-03 11:22:20 -0500

    Delete dist directory

--- dist/assets/haptics-B6hyu8Pm.js
+++
@@ -1,1 +0,0 @@
-const r=(a="light")=>{if("vibrate"in navigator)switch(a){case"light":navigator.vibrate(10);break;case"medium":navigator.vibrate(20);break;case"success":navigator.vibrate([10,30,10]);break;case"warning":navigator.vibrate([30,50,30]);break;case"error":navigator.vibrate([50,100,50,100,50]);break;default:navigator.vibrate(10)}};export{r as t};
